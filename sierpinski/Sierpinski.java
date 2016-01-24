7fc94e30334f57435ba906b5460026730d89254d
Dan Kim <jidankim@gmail.com>
2016-01-24 16:19:40 -0500

    add descriptions

--- sierpinski/Sierpinski.java
+++ sierpinski/Sierpinski.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ *  Compilation:  javac Sierpinski.java
+ *  Execution:    java Sierpinski N
+ *  Dependencies: StdDraw.java
+ *
+ *  Plot an order N Sierpinski Triangle.
+ *
+ *  % java Sierpinski 5
+ *
+ ******************************************************************************/
+
 public class Sierpinski {
 
 	public static void main(String[] args) {
@@ -14,19 +25,27 @@ public class Sierpinski {
 		sierpinski(N, x, y, side);
 	}
 
+	// O(n)
+	// pre: n >= 0, 0 <= x <= 1, 0 <= y <= 1, side <= 0.5
+	// post: draws sierpinski triangles of order n	
 	public static void sierpinski(int n, double x, double y, double side) {	
 		if (n == 0) return;
-		
+		triangle(x, y, side);
+
 		double t = side * Math.sqrt(3.0)/2;
 		
-		triangle(x, y, side);
-		sierpinski(n-1, x, y + t, side/2);
-		sierpinski(n-1, x + side/2, y, side/2);
-		sierpinski(n-1, x - side/2, y, side/2);
+		// recursively draw 3 half-size Sierpinski of order n-1	
+		sierpinski(n-1, x, y + t, side/2); // top 
+		sierpinski(n-1, x + side/2, y, side/2); // left bottom
+		sierpinski(n-1, x - side/2, y, side/2); // right bottom
 	}
 
+	// O(1)
+	// pre: 0 <= startX <= 1, 0 <= startY <= 1, side <= 0.5
+	// post: draws a triangle with a vertex at (startX, startY) and given side
 	public static void triangle(double startX,double startY,double side) {
 		double t = side * Math.sqrt(3.0)/2;
+
 		double[] xd = {startX, startX - side/2, startX + side/2};
 		double[] yd = {startY, startY + t, startY + t};
 		StdDraw.filledPolygon(xd, yd); // draw a triangle 
