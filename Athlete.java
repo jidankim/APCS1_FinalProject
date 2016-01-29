15ef1222cdaddc247ee626e9565287affab08ef2
Dan Kim <jidankim@gmail.com>
2016-01-28 21:00:02 -0500

    fix pull

---
+++ Athlete.java
@@ -0,0 +1,9 @@
+public class Athlete{
+public void Shout(){
+System.out.println("hey i'm an athlete.");
+}
+public static void main(String[] args){
+	Athlete a = new SkiJumper();
+	a.Shout();
+}
+}
---
+++ SkiJumper.java
@@ -0,0 +1,12 @@
+public class SkiJumper extends Athlete{
+private int numberOfJumps;	
+public void Shout(){
+System.out.println("I'm dan kim");
+}
+public SkiJumper(){
+	numberOfJumps = 0;
+	}
+public int getJumps(){
+	return numberOfJumps;
+}
+}
