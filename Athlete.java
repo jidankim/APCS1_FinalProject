0642d5aac43533a9886fc4d3a47e1c5323986b7e
Dan Kim <jidankim@gmail.com>
2016-01-28 21:01:17 -0500

    real

--- Athlete.java
+++
@@ -1,9 +0,0 @@
-public class Athlete{
-public void Shout(){
-System.out.println("hey i'm an athlete.");
-}
-public static void main(String[] args){
-	Athlete a = new SkiJumper();
-	a.Shout();
-}
-}
--- CampaignTest.java
+++ CampaignTest.java
@@ -9,9 +9,9 @@ public class CampaignTest {
     a.straightTalk(500000);
     b.attackAd(1000000);
     a.holdRally(300000);
-    System.out.println("Alexander Hamilton's funding: " + a.getFunds());
+    System.out.println("Alexander Hamilton's funding: $" + a.getFunds());
     System.out.println("Percentage of people who will vote for Alexander Hamilton: " + a.getSupport() + " %");
-    System.out.println("Sally's # jumps: " + b.getFunds());
+    System.out.println("Benjamin Franklin's funding: $" + b.getFunds());
     System.out.println("Percentage of people who will vote for Benjamin Franklin: " + b.getSupport() + " %");
     System.out.println("Benjamin Franklin polls higher than Alexander Hamilton?  " + (b.compareTo(a) > 0));
   }
